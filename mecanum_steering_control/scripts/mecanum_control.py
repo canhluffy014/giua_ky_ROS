558419ca87e1aee52d96735991b4aa443650d0a3
Canh Luffy <canhluffy014@example.com>
2025-04-02 11:10:22 +0700

    Thay thế toàn bộ file bằng phiên bản mới

--- mecanum_steering_control/scripts/mecanum_control.py
+++ mecanum_steering_control/scripts/mecanum_control.py
@@ -8,6 +8,9 @@ class JointStateListener:
         rospy.init_node("joint_states_listener", anonymous=True)
         rospy.Subscriber("/joint_states", JointState, self.joint_states_callback)
 
+        # Publisher cho encoder velocities
+        self.encoder_pub = rospy.Publisher("/encoder_velocities", JointState, queue_size=10)
+
         # Lưu giá trị vận tốc cũ
         self.encoder_velocities = {
             "encoder_left_forward_joint": 0.0,
@@ -16,30 +19,37 @@ class JointStateListener:
             "encoder_right_forward_joint": 0.0
         }
 
-        # Thời gian khi lần in cuối cùng
         self.last_log_time = rospy.Time.now()
-
         rospy.loginfo("Listening to /joint_states topic...")
 
     def joint_states_callback(self, msg):
         current_time = rospy.Time.now()
-        
-        # Kiểm tra nếu đã qua 1 giây kể từ lần in trước
+
+       
         if (current_time - self.last_log_time).to_sec() >= 1.0:
             rospy.loginfo("Received joint states:")
 
             for joint_name in self.encoder_velocities.keys():
-                try:
+                if joint_name in msg.name:
                     index = msg.name.index(joint_name)
-                    velocity = msg.velocity[index] if index < len(msg.velocity) else self.encoder_velocities[joint_name]
-                except ValueError:
-                    velocity = self.encoder_velocities[joint_name]  # Dùng giá trị cũ nếu thiếu dữ liệu
-
+                    if len(msg.velocity) > index:  
+                        velocity = msg.velocity[index]
+                    else:
+                        velocity = 0.0  
+                else:
+                    velocity = 0.0  
                 self.encoder_velocities[joint_name] = velocity
                 rospy.loginfo(f"{joint_name}: Velocity={velocity:.6f}")
-            
-            # Cập nhật thời gian khi đã in
+
+            # Cập nhật thời gian log lần cuối
             self.last_log_time = current_time
+
+            # Gửi velocity tới topic /encoder_velocities
+            encoder_msg = JointState()
+            encoder_msg.name = list(self.encoder_velocities.keys())
+            encoder_msg.velocity = list(self.encoder_velocities.values())
+            encoder_msg.effort = [0.0] * len(encoder_msg.name)
+            self.encoder_pub.publish(encoder_msg)
 
     def run(self):
         rospy.spin()
